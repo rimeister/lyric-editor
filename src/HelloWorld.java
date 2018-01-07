79aae615f5a31659ef0017b0bb8d068294dcec69
Riley Boyd <contact@rileyboyd.com>
2018-01-07 17:20:25 -0500

    Adding folder structure created with Eclipse

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,10 @@
-// Hello world java class
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// Print Hello World in the terminal
 		System.out.println("Hello, World");
+
 	}
-	
+
 }
