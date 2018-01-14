08f78b42e10d868b183e823d88a4d3dccc1dc64e
Riley Boyd <contact@rileyboyd.com>
2018-01-13 20:02:52 -0500

    Adding text edit example I found here: http://www.conanhughes.com/2013/11/how-to-create-text-editor-like-notepad.html. I ran it, it works

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		HelloWorld helloObject = new HelloWorld();
 		helloObject.sayHello();
-	}
+	}*/
 }
